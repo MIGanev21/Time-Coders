19533c6a6d903bcbd00b9efffa266a7c5271966f
Lazar Apostolov <LPApostolov21@codingburgas.bg>
2024-01-07 19:56:08 +0200

    Add the first window

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -1,21 +1,35 @@
-// Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "raylib.h"
 
-#include <iostream>
-#include "raylib.h" 
-
-int main()
+void ToggleFullScreeWindow(int windowWidth, int windowHeight)
 {
-    std::cout << "Hello World!\n";
+    if (!IsWindowFullscreen())
+    {
+        int monitor = GetCurrentMonitor();
+        SetWindowSize(GetMonitorWidth(monitor), GetMonitorHeight(monitor));
+        ToggleFullscreen();
+    }
+    else
+    {
+        ToggleFullscreen();
+        SetWindowSize(windowWidth, windowHeight);
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 800;
+
+    InitWindow(screenWidth, screenHeight, "first window");
+
+    while (!WindowShouldClose())   
+    {
+        if (IsKeyPressed(KEY_F))
+            ToggleFullScreeWindow(screenWidth, screenHeight);
+        BeginDrawing();
+        ClearBackground(LIME);
+        DrawText("I am in your room, under your bed!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+}
